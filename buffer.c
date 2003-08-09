a3d4d4e42ba75dba5a776e3d1ad8136c81cc0594
jaredy <>
2003-08-09 23:16:46 +0000

    lots of fixups, beautify, comments

--- buffer.c
+++ buffer.c
@@ -8,20 +8,26 @@
 
 static void Buffer_ensure(Buffer *p, size_t len);
 
+/*
+ * Frees a buffer. Some pointers may point to
+ * our data member, so there is an argument to
+ * free that field or not.
+ *
+ * Note that Buffer_free() is a macro to NOT
+ * save the data pointer.
+ */
 void Buffer_long_free(Buffer **p, bool saveptr)
 {
 	if (p && *p) {
-		if ((*p)->data && !saveptr) {
+		if ((*p)->data && !saveptr)
 			free((*p)->data);
-//			(*p)->data = NULL;
-		}
-//bark("data freed");
-//		(*p)->mem = 0;
-//		(*p)->len = -1;
-//bark("freeing pointer");
+		/*
+		 * There is no point in clearing the
+		 * fields here (data, mem, len) because
+		 * it won't be used anymore anyhow.
+		 */
 		free(*p);
 		*p = NULL;
-//bark("buffer freed");
 	}
 }
 
@@ -37,59 +43,63 @@ Buffer *Buffer_init(size_t len)
 	p->data = NULL;
 	p->len = -1;
 	p->mem = 0;
-//bark("ensuring %d", len);
 	Buffer_ensure(p, len);
 	/*
 	 * Buffer_ensure() sets len to 0 for future
-	 * invocations, so we must set it again
+	 * invocations to it, so we must reset len again.
 	 */
 //	p->len = -1;
 	return p;
 }
 
+/*
+ * Remove a section of a buffer and replace with another
+ * buffer.
+ */
 void Buffer_replace(Buffer *dst, int start, int len, Buffer *src)
 {
 	int oldlen, newlen;
 	oldlen = Buffer_length(dst);
 	newlen = oldlen - len + Buffer_length(src);
 	Buffer_ensure(dst, newlen);
-	/* move check after target area into safe zone */
-	memmove(dst->data+start+Buffer_length(src), dst->data+start+len,
-		oldlen-(start+len));
-	memcpy(dst->data+start, src->data, Buffer_length(src));
+	/*
+	 * Move remaining buffer to 'safe zone' so it won't get
+	 * clobbered with memcpy() next.
+	 */
+	memmove(dst->data + start + Buffer_length(src),
+		dst->data + start + len, oldlen - (start + len));
+	memcpy(dst->data + start, src->data, Buffer_length(src));
 }
 
+/*
+ * Add a single character to a buffer.
+ */
 void Buffer_addch(Buffer *p, char ch)
 {
 	int len = p->len;
-	/* our data is already zero-bounded */
+	/* Our data is already zero-bounded */
 	if (ch == '\0')
 		return;
 	if (len <= 0) {
-		/* buffer hasn't been initialized yet */
-//bark("uninitialized!");
+		/* Buffer isn't initialized */
 		p->len = len = 1; /* new '\0' */
 	}
-//bark("len: %d (adding %c after %c)", len, ch, p->data[len-2]);
-	Buffer_ensure(p, len+1);
-	p->data[len-1] = ch;
+	Buffer_ensure(p, len + 1);
+	p->data[len - 1] = ch;
 	p->data[len] = '\0';
 	p->len++;
-//fprintf(stderr, "%c", ch);fflush(stderr);
 }
 
 void Buffer_append(Buffer *dst, Buffer *src)
 {
 	if (dst->len > 0) {
 		int len = Buffer_length(dst) + Buffer_length(src) - 1;
-//bark("requires %d [%s%s (%d:%d)]", len, Buffer_get(dst), Buffer_get(src),
-//			Buffer_length(dst), Buffer_length(src));
 		Buffer_ensure(dst, len);
-		strncat(dst->data, src->data, Buffer_length(src)-1);
-		dst->data[len-1] = '\0';
+		strncat(dst->data, src->data, Buffer_length(src) - 1);
+		dst->data[len - 1] = '\0';
 		dst->len = len;
 	} else {
-		/* buffer hasn't been initialized yet */
+		/* Buffer isn't initialized, just copy */
 		Buffer_copy(dst, src);
 	}
 }
@@ -103,28 +113,27 @@ void Buffer_copy(Buffer *dst, Buffer *src)
 
 void Buffer_set(Buffer *p, char *s)
 {
-	int len = strlen(s)+1;
-//bark("ensuring to %d (len: %d mem: %d)", len, p->len, p->mem);
+	int len = strlen(s) + 1; /* '\0' */
 	Buffer_ensure(p, len);
-	strncpy(p->data, s, len-1);
-	p->data[len-1] = '\0';
-//bark("buffer set to %s", p->data);
+	strncpy(p->data, s, len - 1);
+	p->data[len - 1] = '\0';
 	p->len = len;
 }
 
+/*
+ * Concatenate a char array onto a buffer.
+ */
 void Buffer_cat(Buffer *p, char *s)
 {
 	if (p->len >= 0)
 	{
-		int len = p->len+strlen(s);
+		int len = p->len + strlen(s);
 		Buffer_ensure(p, len);
-		strncat(p->data, s, strlen(s)+1);
-//bark("catting buffer with %s", s);
-		p->data[len-1] = '\0';
+		strncat(p->data, s, strlen(s));
+		p->data[len - 1] = '\0';
 		p->len = len;
 	} else {
-		/* buffer hasn't been initialized yet */
-//bark("uninitialized buffer catted!");
+		/* Buffer isn't initialized */
 		Buffer_set(p, s);
 	}
 }
@@ -141,18 +150,19 @@ static void Buffer_ensure(Buffer *p, size_t len)
 {
 	int newlen;
 	if (p->mem < len) {
-//bark("buffer (%s) not big enough, resizing", p->data);
+		/* Round to 8-byte boundary for some poor malloc()'s */
+#if 0
 		newlen = 8*ceil((double)(1+(len * 3)/2)/8); /* 1/log(len) */
-//bark("increasing capacity from %d <len %d> to %d (%d)", p->mem, p->len, newlen, len);
+#endif
+		len = (len * 3) / 2;
+		newlen = (len & ~0x7) + (len & 0x7 ? 8 : 0);
 		assert((p->data = (char *)realloc(p->data, newlen)) != NULL);
 		p->mem = newlen;
-//bark("mem now %d", p->mem);
 		if (p->len < 0) {
 			p->len = 0;
 			p->data[0] = '\0';
 		}
-//bark("writing %d zeros at %p (%c)", newlen-p->len, p->data+p->len, p->data[p->len-1]);
-		bzero(p->data+p->len, newlen-p->len);
+		bzero(p->data + p->len, newlen - p->len);
 	}
 }
 
@@ -163,15 +173,16 @@ bool Buffer_is_set(Buffer *p)
 
 void Buffer_cat_range(Buffer *p, char *start, char *end)
 {
-	int len = p->len + end-start;
-//bark("[len: %d] [mem: %d] [data: %s]", p->len, p->mem, p->data);
+	int len = p->len + end - start;
 	Buffer_ensure(p, len);
-	strncat(p->data, start, end-start);
-	p->data[len-1] = '\0';
+	strncat(p->data, start, end - start);
+	p->data[len - 1] = '\0';
 	p->len = len;
-//bark("[len: %d] [mem: %d] [data: %s]", p->len, p->mem, p->data);
 }
 
+/*
+ * Buffer vector - array of buffers.
+ */
 VBuffer *VBuffer_init(void)
 {
 	VBuffer *v;
@@ -181,7 +192,7 @@ VBuffer *VBuffer_init(void)
 	return v;
 }
 
-/*
+#if 0
 void VBuffer_clear(VBuffer **v)
 {
 	VBuffer **i;
@@ -193,8 +204,15 @@ void VBuffer_clear(VBuffer **v)
 		}
 	}
 }
-*/
+#endif
 
+/*
+ * Frees a buffer vector. The second argument specifies
+ * whether or not to free the associated buffers.
+ *
+ * Note that VBuffer_free() is a macro for this to not
+ * save the buffers.
+ */
 void VBuffer_long_free(VBuffer **v, bool save)
 {
 	if ((v != NULL) && (*v != NULL)) {
@@ -205,46 +223,54 @@ void VBuffer_long_free(VBuffer **v, bool save)
 	}
 }
 
+/*
+ * Add a buffer to a buffer vector.
+ */
 void VBuffer_add(VBuffer *v, Buffer *p)
 {
 	VBuffer *new, *i;
 	if (v->buf == NULL) {
-		/* it's empty, use it */
+		/* If this node is empty, use it */
 		v->buf = p;
 	} else {
 		new = VBuffer_init();
 		new->buf = p;
-		/* find last vbuf */
+		/* Find last vbuf */
 		for (i = v; i->next != NULL; i = i->next);
 		i->next = new;
-bark("[vbuf] added %s", Buffer_get(p));
 	}
 }
 
+/*
+ * Remove a buffer from a buffer vector.
+ */
 Buffer *VBuffer_remove(VBuffer **v)
 {
 	VBuffer *newlast = NULL, *last;
 	Buffer *p;
+	/*
+	 * We need to find and return the last buffer and
+	 * make the second-last buffer the new last buffer.
+	 */
 	for (last = *v; last && (last->next != NULL); last = last->next)
 		newlast = last;
-//bark("[vbuf] second-last: [%s] last: [%s]",
-//	newlast == NULL ? "(NULL)" : Buffer_get(newlast->buf),
-//	last    == NULL ? "(NULL)" : Buffer_get(last->buf));
-	/* tried to remove an empty vbuf */
+	/* This function was called on an empty vbuf */
 	if ((newlast == NULL) && (last == NULL))
 		return NULL;
 	p = last->buf;
 	if (newlast == NULL) {
-		/* last one, clear original vbuf */
+		/* This is the last buf; clear original vbuf */
 		VBuffer_long_free(v, TRUE);
 	} else {
 		newlast->next = NULL;
 		VBuffer_long_free(&last, TRUE);
 	}
-//bark("vbuf freed");
 	return p;
 }
 
+/*
+ * Find the number of buffers in a buffer vector.
+ */
 size_t VBuffer_length(VBuffer *v)
 {
 	VBuffer *i;
